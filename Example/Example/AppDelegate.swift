69f09de72f323c4cd59ecae951e5ff520a82c332
LCD <565726319@qq.com>
2020-04-27 09:50:11 +0800

    🔧 LCD -

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -27,6 +27,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         return composite.application(app, open: url, options: options)
     }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        return true
+    }
 }
 
 
